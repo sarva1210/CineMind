e9b7ee15b757cc98b5794e6f75a58e37793d77f3
sarva1210 <sarva.vish77@gmail.com>
2026-05-14 21:30:32 +0530

    db

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -9,6 +9,7 @@ const connectDB = async () => {
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
     return conn;
+    
   } catch (error) {
     console.error(`Error connecting to MongoDB: ${error.message}`);
     process.exit(1);
