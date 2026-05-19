600e763bb76e2467801d32ca0088f952a27d5a51
sarva1210 <sarva.vish77@gmail.com>
2026-05-19 23:31:40 +0530

    routes

--- backend/src/modules/auth/authRoutes.js
+++ backend/src/modules/auth/authRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { register, login } = require('./authController');
 
+
 // POST /auth/register
 router.post('/register', register);
 
