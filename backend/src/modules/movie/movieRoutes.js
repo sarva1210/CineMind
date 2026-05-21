dc76046c8777e4e009136e3ec9290d93f70aa90d
sarva1210 <sarva.vish77@gmail.com>
2026-05-21 22:27:11 +0530

    routes

--- backend/src/modules/movie/movieRoutes.js
+++ backend/src/modules/movie/movieRoutes.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const {
-  getTrendingMovies,
-  searchMovies,
+  getTrendingMovies, searchMovies,
   getMovieDetails, getMovieTrailer,
   getPopularMovies, getTopRatedMovies } = require('./movieController');
 
